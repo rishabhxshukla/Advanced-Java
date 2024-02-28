fc3f0a09c391aaa90d7bb86ae52b1f52d6eeba1c
Rishabh Shukla <rs515666@gmail.com>
2024-02-28 10:35:45 +0530

    Added new files

---
+++ None Match.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class NoneMatch
+{
+    public static void main(String args[])
+    {
+        //Creating a list
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        //Do all elements satisfy the condition
+        boolean ans = numbers
+                .stream()
+                .noneMatch((num) -> num > 5);
+
+        System.out.println(ans);
+    }
+}
