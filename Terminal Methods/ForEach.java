bb4b0bd4897ddbf0afbf418869bcba9b7fc585cd
Rishabh Shukla <rs515666@gmail.com>
2024-02-28 13:25:30 +0530

    Added new files

---
+++ Terminal Methods/ForEach.java
@@ -0,0 +1,14 @@
+package TerminalMethods;
+import java.util.*;
+
+class ForEach
+{
+    public static void main(String args[])
+    {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        numbers
+                .stream()
+                .forEach(System.out::println);
+    }
+}
